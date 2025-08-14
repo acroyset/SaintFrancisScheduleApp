bd2bb7bb1f6dfc69fe951f04c3685947394db1fb
acroyset <95663162+acroyset@users.noreply.github.com>
2025-08-13 18:26:57 -0700

    Initial Commit

---
+++ Schedule/ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleApp.swift
+//  Schedule
+//
+//  Created by Andreas Royset on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScheduleTests/ScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleTests.swift
+//  ScheduleTests
+//
+//  Created by Andreas Royset on 8/13/25.
+//
+
+import Testing
+@testable import Schedule
+
+struct ScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
